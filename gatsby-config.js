cfd4f699aaad1c27f2ea8eed3454667c3a339de3
yoavsbg <yoavsbg17@gmail.com>
2022-12-08 17:43:28 +0200

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,20 @@ module.exports = {
     siteUrl: `https://easy-code.blog/`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "<MEASUREMENT ID>", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
